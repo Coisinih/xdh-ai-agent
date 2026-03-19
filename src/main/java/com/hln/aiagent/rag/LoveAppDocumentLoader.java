4e75790331bf58ed530d873157ee47049268cd34
1549414965@qq.com <1549414965@qq.com>
2026-03-19 20:22:48 +0800

    解析 markdown 文件，并加载到 RAG 中

---
+++ src/main/java/com/hln/aiagent/rag/LoveAppDocumentLoader.java
@@ -0,0 +1,49 @@
+package com.hln.aiagent.rag;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.reader.markdown.MarkdownDocumentReader;
+import org.springframework.ai.reader.markdown.config.MarkdownDocumentReaderConfig;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.ResourcePatternResolver;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 实现解析 markdown 文件，并加载到 RAG 中
+ */
+@Component
+@Slf4j
+public class LoveAppDocumentLoader {
+    private final ResourcePatternResolver resourcePatternResolver;
+
+    public LoveAppDocumentLoader(ResourcePatternResolver resourcePatternResolver) {
+        this.resourcePatternResolver = resourcePatternResolver;
+    }
+
+    public List<Document> loadDocuments() {
+        List<Document> documents = new ArrayList<>();
+        try {
+            Resource[] resources = resourcePatternResolver.getResources("classpath:document/*.md");
+            for (Resource resource : resources) {
+                String filename = resource.getFilename();
+                MarkdownDocumentReaderConfig config = MarkdownDocumentReaderConfig.builder()
+                        .withHorizontalRuleCreateDocument(true)
+                        .withIncludeCodeBlock(false)
+                        .withIncludeBlockquote(false)
+                        .withAdditionalMetadata("filename", filename)
+                        .build();
+
+                List<Document> reads = new MarkdownDocumentReader(resource, config).read();
+                documents.addAll(reads);
+            }
+        } catch (IOException e) {
+            log.error("Error loading documents: ", e);
+        }
+        return documents;
+    }
+}
